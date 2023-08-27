7e24bc41f45d8492d81990aacc33e19906f8a392
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-08-27 20:53:09 -0300

    [feat] add getters

--- src/domain/entity/Person.py
+++ src/domain/entity/Person.py
@@ -11,9 +11,29 @@ class Person:
     __nascimento: str
     __stack:  Optional[List[str]]
 
-    def __init__(self, id_: uuid, apelido: str, nome: str, nascimento: str, stack: Optional[List[str]]):
-        self.__id_ = id_
+    def __init__(self, apelido: str, nome: str, nascimento: str, stack: Optional[List[str]]):
+        self.__id_ = uuid.uuid4()
         self.__apelido = apelido
         self.__nome = nome
         self.__nascimento = nascimento
         self.__stack = stack
+   
+    @property
+    def id(self):
+        return self.__id_
+
+    @property
+    def apelido(self):
+        return self.__apelido
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def nascimento(self):
+        return self.__nascimento
+
+    @property
+    def stack(self):
+        return self.__stack
