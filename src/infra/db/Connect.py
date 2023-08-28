77cadc319e81a25cabecaafd3e78fb060e7a65f9
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-08-28 18:21:51 -0300

    [fix] erros in the asyncpg

--- src/infra/db/Connect.py
+++ src/infra/db/Connect.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 import asyncpg
 import os
 from dotenv import load_dotenv
@@ -9,24 +10,45 @@ class PostgresDatabase:
         self.config = {
             "host": os.getenv("POSTGRES_HOST"),
             "port": os.getenv("POSTGRES_PORT"),
-            "database": os.getenv("POSTGRES_DB"),  # Note: asyncpg uses "database" instead of "dbname"
+            "database": os.getenv("POSTGRES_DB"),
             "user": os.getenv("POSTGRES_USER"),
             "password": os.getenv("POSTGRES_PASSWORD"),
-            "timeout": 5  # asyncpg uses "timeout" instead of "connect_timeout"
+            "min_size": 5,  # Minimum number of connections in the pool
+            "max_size": 20,  # Maximum number of connections in the pool
+            "timeout": 10  # Connection timeout
         }
-        self.conn = None
+        self.pool = None
 
     async def connect(self):
-        try:
-            self.conn = await asyncpg.connect(**self.config)
-            print("Connected to the PostgreSQL database successfully!")
-        except Exception as e:
-            print(f"Error connecting to the database: {e}")
+        self.pool = await asyncpg.create_pool(**self.config)
+        print("Connection pool created successfully!")
 
     async def close(self):
-        if self.conn:
-            await self.conn.close()
-            print("Database connection closed.")
+        await self.pool.close()
+        print("Connection pool closed.")
+
+    async def get_conn(self):
+        return await self.pool.acquire()
+
+    async def release_conn(self, conn):
+        await self.pool.release(conn)
+    
+    async def __aenter__(self):
+        self.conn = await self.pool.acquire()
+        return self.conn
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.pool.release(self.conn)
+    
+    async def fetchval(self, query, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetchval(query, *args)
+    async def fetchrow(self, query, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetchrow(query, *args)
+    async def fetch(self, query, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetch(query, *args)
 
 async def get_db():
     db = PostgresDatabase()
--- src/infra/repository/PersonRepository.py
+++ src/infra/repository/PersonRepository.py
@@ -6,42 +6,50 @@ from fastapi import HTTPException
 
 class PersonRepository:
 
-    def __init__(self, db_connection):
-        self.db = db_connection
-        self.insert_person_stmt = None
-
-
-    async def prepare_statements(self):
-        self.insert_person_stmt = await self.db.conn.prepare(
-            "INSERT INTO pessoas (id, apelido, nome, nascimento, stack) VALUES ($1, $2, $3, $4, $5)"
-        )
+    def __init__(self, db_pool):
+        self.db_pool = db_pool
 
     async def get_person_by_apelido(self, apelido: str):
-        result = await self.db.conn.fetchrow("SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE apelido = $1", apelido)
-        if result:
-            return Person(result['apelido'], result['nome'], result['nascimento'], result['stack'])
+        async with self.db_pool as conn:
+            result = await conn.fetchrow("SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE apelido = $1", apelido)
+            if result:
+                return Person(result['apelido'], result['nome'], result['nascimento'], result['stack'])
         return None
 
     async def add_person(self, person: Person):
-        try:
-            if not self.insert_person_stmt:
-                await self.prepare_statements()
+        async with self.db_pool as conn:
             nascimento_date = datetime.strptime(person.nascimento, '%Y-%m-%d').date()
-
-            await self.insert_person_stmt.fetch(str(person.id), person.apelido, person.nome, nascimento_date, person.stack)
-        except asyncpg.UniqueViolationError:
-            raise HTTPException(status_code=400, detail="Person already exists")
+            try:
+                await conn.execute(
+                    "INSERT INTO pessoas (id, apelido, nome, nascimento, stack) VALUES ($1, $2, $3, $4, $5)",
+                    str(person.id), person.apelido, person.nome, nascimento_date, person.stack
+                )
+            except asyncpg.UniqueViolationError:
+                raise HTTPException(status_code=400, detail="Person already exists")
+            finally:
+                await self.db_pool.release_conn(conn)
         return None
 
+
     async def get_person_by_id(self, person_id: uuid.UUID):
-        result = await self.db.conn.fetchrow("SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE id = $1", person_id)
-        if result:
-            return Person(result['apelido'], result['nome'], result['nascimento'], result['stack'])
+        async with self.db_pool as conn:
+            result = await conn.fetchrow("SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE id = $1", person_id)
+            if result:
+                await self.db_pool.release_conn(conn)
+                return Person(result['apelido'], result['nome'], result['nascimento'], result['stack'])
+
         return None
 
     async def search_person_by_term(self, term: str):
-        results = await self.db.conn.fetch("SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE apelido ILIKE $1 OR nome ILIKE $2 OR $3 = ANY(stack)", f"%{term}%", f"%{term}%", term)
-        return [Person(result['apelido'], result['nome'], result['nascimento'], result['stack']) for result in results]
+        async with self.db_pool as conn:
+            results = await conn.fetch("SELECT id, apelido, nome, nascimento, stack FROM pessoas WHERE apelido ILIKE $1 OR nome ILIKE $2 OR $3 = ANY(stack)", f"%{term}%", f"%{term}%", term)
+            await self.db_pool.release_conn(conn)
+
+            return [Person(result['apelido'], result['nome'], result['nascimento'], result['stack']) for result in results]
 
     async def count_persons(self):
-        return await self.db.conn.fetchval("SELECT COUNT(*) FROM pessoas")
+        async with self.db_pool as conn:
+            result = await conn.fetchval("SELECT COUNT(*) FROM pessoas")
+            await self.db_pool.release_conn(conn)
+
+            return result 
