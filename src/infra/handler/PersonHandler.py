284e6da655afa804240d4b98cfabaed6d2386693
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-08-28 11:09:56 -0300

    [feat] add validation

--- src/infra/handler/PersonHandler.py
+++ src/infra/handler/PersonHandler.py
@@ -2,6 +2,7 @@ import psycopg2
 from pydantic import ValidationError
 from fastapi import HTTPException
 
+from datetime import datetime
 from src.domain.entity.Person import Person
 from src.infra.repository.PersonRepository import PersonRepository
 
@@ -13,6 +14,8 @@ class PessoaHandler:
     def create(self, pessoa_dto):
         try:
             person = Person(pessoa_dto.apelido, pessoa_dto.nome, pessoa_dto.nascimento, pessoa_dto.stack)
+            if not PessoaHandler.__is_valid_date(person.nascimento):
+                raise HTTPException(status_code=400, detail="Invalid date format")
             apelido = self.person_repo.get_person_by_apelido(person.apelido)
             if apelido:
                 raise HTTPException(status_code=400, detail="Apelido already exists")
@@ -41,3 +44,10 @@ class PessoaHandler:
 
     def count(self):
         return {"count": self.person_repo.count_persons()}
+    
+    def __is_valid_date(date_str):
+        try:
+            datetime.strptime(date_str, '%Y-%m-%d')
+            return True
+        except ValueError:
+            return False
